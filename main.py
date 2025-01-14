dba19b78e7851d50049a355f3349b45248182036
Max <maxweissman@rogers.com>
2025-01-14 15:00:37 -0800

    Change player and other circles from drawings to sprites

--- main.py
+++ main.py
@@ -11,14 +11,35 @@ clock = pygame.time.Clock()
 running = True
 dt = 0.05
 
+background = 'purple'
+
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-circles = []
+class Circle(pygame.sprite.Sprite):
+	def __init__(self, color, width, coord):
+		pygame.sprite.Sprite.__init__(self)
 
-# Adds a circle coordinate every second
+		self.image = pygame.Surface([width, width])
+		self.image.fill(background)
+		self.image.set_colorkey(background)
+
+		pygame.draw.circle(self.image, color, (width / 2, width / 2), width / 2)
+
+		self.rect = self.image.get_rect()
+		self.rect.x, self.rect.y = coord
+
+sprites = pygame.sprite.Group()
+
+# initialize player circle
+player = Circle("red", 40, player_pos)
+sprites.add(player)
+
+# Adds a green circle every second
 def circle_position():
 	while True:
-		circles.append(pygame.Vector2(random.randint(0, screen.get_width()), random.randint(0, screen.get_height())))
+		coords = pygame.Vector2(random.randint(0, screen.get_width()), random.randint(0, screen.get_height()))
+		green_circle = Circle("green", 40, coords)
+		sprites.add(green_circle)
 		time.sleep(1)
 
 add_circles = Thread(target=circle_position)
@@ -32,35 +53,32 @@ while running:
 			running = False
 
 	# fill the screen with a color to wipe away anything from last frame
-	screen.fill("purple")
+	screen.fill(background)
 
-	# RENDER YOUR GAME HERE
-	pygame.draw.circle(screen, "red", player_pos, 40)
-
-	# Render existing green circles
-	for circle in circles:
-		pygame.draw.circle(screen, "green", circle, 40)
+	# render sprites
+	sprites.update()
+	sprites.draw(screen)
 
 	# arrow keys move circle
 	keys = pygame.key.get_pressed()
 	if keys[pygame.K_w]:
-		player_pos.y -= 300 * dt
+		player.rect.y -= 300 * dt
 	if keys[pygame.K_s]:
-		player_pos.y += 300 * dt
+		player.rect.y += 300 * dt
 	if keys[pygame.K_a]:
-		player_pos.x -= 300 * dt
+		player.rect.x -= 300 * dt
 	if keys[pygame.K_d]:
-		player_pos.x += 300 * dt
+		player.rect.x += 300 * dt
 
 	# loop circle from edges of screen
-	if player_pos.y <= -40:
-		player_pos.y = screen.get_height() + 30
-	if player_pos.y >= 40 + screen.get_height():
-		player_pos.y = -30
-	if player_pos.x <= -40:
-		player_pos.x = screen.get_width() + 30
-	if player_pos.x >= 40 + screen.get_width():
-		player_pos.x = -30
+	if player.rect.y <= -40:
+		player.rect.y = screen.get_height() + 30
+	if player.rect.y >= 40 + screen.get_height():
+		player.rect.y = -30
+	if player.rect.x <= -40:
+		player.rect.x = screen.get_width() + 30
+	if player.rect.x >= 40 + screen.get_width():
+		player.rect.x = -30
 
 	# flip() the display to put your work on screen
 	pygame.display.flip()
